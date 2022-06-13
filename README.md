7c305c2834c964c97873e13d204782905f546357
Mauxd777 <Mauxd607@gmail.com>
2022-06-12 18:08:24 -0600

    Parte 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ProyectoDeCalidad
  Trabajo de Calidad de software
+![Image text](src/ test/ java/ Imagenes/ Selenium.png)
+Hola esto es olo una prueba
--- src/test/java/com/qualitystrem/tutorial/GoogleSearchTests.java
+++ src/test/java/com/qualitystrem/tutorial/GoogleSearchTests.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.concurrent.TimeUnit;
 
+import javax.swing.JOptionPane;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,18 +25,19 @@ public class GoogleSearchTests {
 		System.setProperty("webdriver.chrome.driver","./src/test/resources/Chromedriver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.google.com/");
+		driver.get("http://automationpractice.com/index.php");
 		//esto solo es para que no se cierre el navegador
 	}
 	
 	@Test
 	public void testeGooglePage() {
-		WebElement searchbox = driver.findElement(By.name("q"));
+		WebElement searchbox = driver.findElement(By.name("search_query"));
 		searchbox.clear();
 		searchbox.sendKeys("Esto es una prueba");
 		searchbox.submit();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		assertEquals("Esto es una prueba - Buscar con Google", driver.getTitle());
+		assertEquals("Search - My Store", driver.getTitle());
+		JOptionPane.showMessageDialog(null,"Esta pura prueba sale bien");
 	}
 	@After
 	public void tearDow() {
